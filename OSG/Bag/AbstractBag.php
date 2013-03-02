e947b269aad677889d093b02f71611ff09f7780a
Geoffrey Oslund <geoff@cllcmedia.com>
2013-03-02 13:31:35 -0700

    typo

--- OSG/Bag/AbstractBag.php
+++ OSG/Bag/AbstractBag.php
@@ -2,7 +2,16 @@
 namespace OSG\Bag;
 
 abstract class AbstractBag {
+	
+	/*
+		bag will be an array by default for php ease,
+		but it will be able to take in either an array or a json object.
+	*/
 	private $bag = array();
+
+	public function __construct($json_string = array()) {
+		if()
+	}
 
 	public function addBag(AbstractBag $bag) {
 		$this->bag->push($bag);
@@ -11,8 +20,7 @@ abstract class AbstractBag {
 
 	public function removeBag(AbstractBag $bag) {
 		for ($i = 0; $i < sizeof($this->bag); $i++) {
-			var_dump($bag);
-			die();
+			
 		}
 	}
 
